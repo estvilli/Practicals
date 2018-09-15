60186928115f4fec2a658effa4c44af7144568a1
williamstraus <wllam08@gmail.com>
2018-09-15 14:11:23 +0300

    created repo

---
+++ Domain/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            
+        }
+        
+        public Person(string firstName, string lastName, string nickname)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Nickname = nickname;
+        }
+
+        /*public override string toString()
+        {
+            if (string.IsNullOrEmpty(Nickname))
+            {
+                return FirstName + " " + LastName;
+            }
+            return FirstName + " \"" + Nickname + "\" " + LastName;
+        }*/
+    }
+}
---
+++ Prax01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Domain;
+
+namespace Prax01
+{
+    public class Class1
+    {
+        static void main(String[] args)
+        {
+            Person person1 = new Person("William", "Straus");
+            Person person2 = new Person("Tero", "Taskila", "Teroste");
+            
+            Console.WriteLine("Person1 FirstName: " + person1.FirstName + " LastName: " + person1.LastName);
+            Console.WriteLine("Person: " + person2);
+            Console.WriteLine("Person: " + person1);
+        }
+    }
+}
